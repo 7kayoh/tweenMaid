6e386af640acdd7788c16794c02d08e71d7aee4e
va1kio <40730127+va1kio@users.noreply.github.com>
2021-02-04 18:52:53 +0900

    Fixed server.lua and client.lua

--- client.lua
+++ client.lua
@@ -1,8 +1,8 @@
 local TweenService = game:GetService("TweenService")
 local Event = game:GetService("ReplicatedStorage"):WaitForChild("tweenEvent")
 
-Event.OnClientEvent:Connect(function(GUID, Object, TweenInfo, Goal)
-	TweenService:Create(Object, TweenInfo, Goal):Play()
-	wait(TweenInfo.Time)
+Event.OnClientEvent:Connect(function(GUID, Object, InfoTable, Goal)
+	TweenService:Create(Object, TweenInfo.new(table.unpack(InfoTable)), Goal):Play()
+	wait(InfoTable[1])
 	Event:FireServer(GUID)
 end)
--- server.lua
+++ server.lua
@@ -2,24 +2,24 @@ local HttpService = game:GetService("HttpService")
 local Event = game:GetService("ReplicatedStorage").tweenEvent
 local t = {}
 
-function registerTweening(Object, TweenInfo, Goal)
+function registerTweening(Object, InfoTable, Goal)
 	local GUID = HttpService:GenerateGUID()
 	t[GUID] = {
 		["Object"] = Object,
 		Start = tick(),
-		Duration = TweenInfo.Time,
+		Duration = InfoTable[1],
 		["Goal"] = Goal
 	}
 	
-	Event:FireAllClients(GUID, Object, TweenInfo, Goal)
+	Event:FireAllClients(GUID, Object, InfoTable, Goal)
 end
 
-Event.OnServerEvent:Connect(Client, GUID)
+Event.OnServerEvent:Connect(function(Client, GUID)
 	if t[GUID] and tick() - t[GUID].Start >= t[GUID].Duration then
 		for i,v in pairs(t[GUID].Goal) do
 			t[GUID].Object[i] = v
 		end
-		
+
 		t[GUID] = nil
 	end
 end)
