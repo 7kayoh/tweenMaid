eda6407dd346109ab3b63c2ec62bcc319786d719
nanako <40730127+va1kio@users.noreply.github.com>
2021-03-06 21:58:59 +0800

    #1

--- client.lua
+++ client.lua
@@ -1,8 +1,5 @@
 local TweenService = game:GetService("TweenService")
-local Event = game:GetService("ReplicatedStorage"):WaitForChild("tweenEvent")
 
 Event.OnClientEvent:Connect(function(GUID, Object, InfoTable, Goal)
 	TweenService:Create(Object, TweenInfo.new(table.unpack(InfoTable)), Goal):Play()
-	wait(InfoTable[1])
-	Event:FireServer(GUID)
 end)
