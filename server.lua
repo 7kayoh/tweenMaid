81ebe0f6886c396cc3dec08f70ab7760afbb64be
nanako <40730127+va1kio@users.noreply.github.com>
2021-03-06 21:58:41 +0800

    #1

--- server.lua
+++ server.lua
@@ -1,25 +1,9 @@
 local HttpService = game:GetService("HttpService")
-local Event = game:GetService("ReplicatedStorage").tweenEvent
-local t = {}
 
 function registerTweening(Object, InfoTable, Goal)
-	local GUID = HttpService:GenerateGUID()
-	t[GUID] = {
-		["Object"] = Object,
-		Start = tick(),
-		Duration = InfoTable[1],
-		["Goal"] = Goal
-	}
-	
-	Event:FireAllClients(GUID, Object, InfoTable, Goal)
-end
-
-Event.OnServerEvent:Connect(function(Client, GUID)
-	if t[GUID] and tick() - t[GUID].Start >= t[GUID].Duration then
-		for i,v in pairs(t[GUID].Goal) do
-			t[GUID].Object[i] = v
-		end
-
-		t[GUID] = nil
+	Event:FireAllClients(Object, InfoTable, Goal)
+	wait(InfoTable[1)
+	for i,v in pairs(Goal) do
+		Object[i] = v
 	end
-end)
+end
